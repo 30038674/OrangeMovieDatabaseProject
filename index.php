d250aef9e2d080c9b5e1c4251a4cb41286f311a1
Ben <runenighter@gmail.com>
2021-11-07 13:23:11 +0800

    Updating Adaptive code

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
-	<!-- Benjamin Wearn - P467480 -->
-	<!-- Template -->
+	<!-- Team Orange: Web Movie Database -->
+	<!-- Team: Ben, Jaron, Chris-->
+	<!-- original project base: Ben -->
 	<!--  -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	
 	
 	<head>
 		<!-- Code Comment -->
