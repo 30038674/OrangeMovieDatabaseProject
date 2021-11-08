5bd347805d7ddcf9eb32e93ac8bcc187fa11f951
Ben <runenighter@gmail.com>
2021-11-08 09:30:16 +0800

    Update index.php
    Formatting changes after html code validation and updating mobile display errors.

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
+<html lang="en">
 	<!-- Team Orange: Web Movie Database -->
 	<!-- Team: Ben, Jaron, Chris-->
 	<!-- original project base: Ben -->
+	<!-- Version: 1.0 (First Release) -->
+	<!-- Version History: 
+	0.3 Fixed graphical display issues for mobile browsers - Labels misplaced on mobile form factor
+	0.2 Added adaptive code
+	0.1 Base implementation from previous project -->
 	<!--  -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	
 	
 	
 	<head>
-		<!-- Code Comment -->
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 		<title>Home</title>
 	</head>
 
@@ -18,23 +24,22 @@
 			<li><a href="bargraph.php">Top10</a></li>
 		</ul>
 		
-		<h1>SMT Movie Database</h1>
+		
 		
 		<main>
-			<article class="col-lg-12" align='center'>
+			<article class="col-lg-12">
+            	<h1>SMT Movie Database</h1>
 				<br>
-				<p>Use the fields below to search our database!</P>
-				
+				<center><p>Use the fields below to search our database!</p></center>
+					<!-- Form for database searching -->
 					<form action="searchscr.php" method="post">
                         <div class="form-group col-lg-3">
-							<label for="title">Title:</label>
-							<input type="text" class="form-control" id="title" name="title">
-							<label for="year">Year:</label>
-							<input type="text" class="form-control" id="year" name="year">
-							<br>
-							<label for="rating">Rating:</label>
-							<input type="text" class="form-control" list="rating_list" id="rating" name="rating">
-								<datalist id="rating_list">
+                        	<p style="text-align: center;">Title:&nbsp;&nbsp;<input type="text" class="form-control" id="title" name="title" /></p>
+							<p style="text-align: center;">Year:&nbsp;&nbsp;<input type="text" class="form-control" id="year" name="year" /></p>
+                        	<p style="text-align: center;">Rating:&nbsp;&nbsp;<input type="text" class="form-control" list="rating_list" id="rating" name="rating" /></p>
+                        	<p style="text-align: center;">Genre:&nbsp;&nbsp;<input type="text" class="form-control" list="genre_list" id="genre" name="genre" /></p>
+								<!-- Datalist, allows dropdown box for inputs where inputs may not be known (database construction) -->
+                        		<datalist id="rating_list">
 									<option value="G"></option>
 									<option value="NC-17"></option>
 									<option value="NR"></option>
@@ -48,9 +53,7 @@
 									<option value="UR/R"></option>
 									<option value="VAR"></option>
 								</datalist>
-								
-							<label for="genre">Genre:</label>
-							<input type="text" class="form-control" list="genre_list" id="genre" name="genre">
+								<!-- Another datalist -->
 								<datalist id="genre_list">
 									<option value="Action/Adventure"></option>
 									<option value="Action/Comedy"></option>
@@ -86,7 +89,8 @@
                         </div>
                         <div class="col-lg-10">
 						<br>
-                        <button type="submit" formaction="searchscr.php" class="btn btn-default">Search Database</button>
+                        
+                        <center><button type="submit" formaction="searchscr.php" class="btn btn-default">Search Database</button></center>
                         </div>
 						
                     </form>
